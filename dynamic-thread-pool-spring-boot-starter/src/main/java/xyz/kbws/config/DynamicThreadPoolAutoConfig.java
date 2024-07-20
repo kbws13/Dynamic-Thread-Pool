936b7fb5f459b8946fbc416c824c3343cb46ed45
空白无上 <hsy040506@163.com>
2024-07-20 20:34:38 +0800

    init

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/xyz/kbws/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,9 @@
+package xyz.kbws.config;
+
+/**
+ * @author kbws
+ * @date 2024/7/20
+ * @description: 动态配置入口
+ */
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/test/java/xyz/kbws/ApiTest.java
@@ -0,0 +1,9 @@
+package xyz.kbws;
+
+/**
+ * @author kbws
+ * @date 2024/7/20
+ * @description: 单元测试
+ */
+public class ApiTest {
+}
